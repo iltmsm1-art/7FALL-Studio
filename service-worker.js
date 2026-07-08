2ce56f9335d81f3226fc09fd0ddcd7aa37c263fc
iltmsm1-art <iltmsm1@gmail.com>
2026-07-08 10:06:21 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,14 @@
-const CACHE_NAME='sevenfall-studio-edition-v1-2-1';
-const APP_SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE_NAME='7fall-brand-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/android-192.png',
+  './icons/android-512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-16x16.png',
+  './icons/favicon-32x32.png'
+];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
